10dbc367e65845dca40998e03127e26a6dacbe35
Andrei <andreiuttzzumarian00@gmail.com>
2020-06-19 18:50:58 +0100

    final

--- Chat.Designer.cs
+++ Chat.Designer.cs
@@ -28,17 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(chat));
             this.bSend = new System.Windows.Forms.Button();
             this.tConsole = new System.Windows.Forms.TextBox();
             this.tChat = new System.Windows.Forms.TextBox();
             this.bConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // bSend
             // 
-            this.bSend.Location = new System.Drawing.Point(639, 344);
+            this.bSend.Location = new System.Drawing.Point(822, 447);
             this.bSend.Name = "bSend";
-            this.bSend.Size = new System.Drawing.Size(149, 68);
+            this.bSend.Size = new System.Drawing.Size(310, 105);
             this.bSend.TabIndex = 0;
             this.bSend.Text = "Send!";
             this.bSend.UseVisualStyleBackColor = true;
@@ -49,35 +51,48 @@
             this.tConsole.Location = new System.Drawing.Point(27, 33);
             this.tConsole.Multiline = true;
             this.tConsole.Name = "tConsole";
-            this.tConsole.Size = new System.Drawing.Size(585, 310);
+            this.tConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tConsole.Size = new System.Drawing.Size(585, 392);
             this.tConsole.TabIndex = 1;
             this.tConsole.TabStop = false;
             this.tConsole.TextChanged += new System.EventHandler(this.tConsole_TextChanged);
             // 
             // tChat
             // 
-            this.tChat.Location = new System.Drawing.Point(27, 350);
+            this.tChat.Location = new System.Drawing.Point(27, 447);
             this.tChat.Multiline = true;
             this.tChat.Name = "tChat";
-            this.tChat.Size = new System.Drawing.Size(585, 62);
+            this.tChat.Size = new System.Drawing.Size(719, 105);
             this.tChat.TabIndex = 2;
             this.tChat.TextChanged += new System.EventHandler(this.tChat_TextChanged);
             // 
             // bConnect
             // 
-            this.bConnect.Location = new System.Drawing.Point(639, 35);
+            this.bConnect.Location = new System.Drawing.Point(639, 33);
             this.bConnect.Name = "bConnect";
-            this.bConnect.Size = new System.Drawing.Size(149, 85);
+            this.bConnect.Size = new System.Drawing.Size(493, 85);
             this.bConnect.TabIndex = 3;
             this.bConnect.Text = "Connect to Server";
             this.bConnect.UseVisualStyleBackColor = true;
             this.bConnect.Click += new System.EventHandler(this.bConnect_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(639, 148);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(522, 229);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
             // chat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1173, 582);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.bConnect);
             this.Controls.Add(this.tChat);
             this.Controls.Add(this.tConsole);
@@ -95,5 +110,6 @@
         private System.Windows.Forms.TextBox tConsole;
         private System.Windows.Forms.TextBox tChat;
         private System.Windows.Forms.Button bConnect;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
